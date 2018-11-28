9bea55952383dadfb8c15c1da3bb5465178a3d2b
Shilat Feuchtwanger Myzel <shilat.myzel@gmail.com>
2018-11-28 18:35:43 +0200

    working3

--- bot.py
+++ bot.py
@@ -3,9 +3,7 @@ import logging
 import data
 import mergeimage
 
-from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-from telegram.ext import Updater
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
@@ -21,8 +19,12 @@ updater = Updater(token=secret_settings.BOT_TOKEN)
 dispatcher = updater.dispatcher
 
 
-def start(bot, update):
+dicargs={}
+def start(bot, update, args):
     chat_id = update.message.chat_id
+    if args:
+        dicargs[chat_id] = args
+    print(args)
     logger.info(f"> Start chat #{chat_id}")
     bot.send_message(chat_id=chat_id, text="Welcome !!!")
     keyboard = [[InlineKeyboardButton("Collage", callback_data='Collage'),
@@ -31,6 +33,54 @@ def start(bot, update):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Please choose what you want to do:', reply_markup=reply_markup)
 
+
+def respond(bot, update):
+    chat_id = update.message.chat_id
+    text = update.message.text
+    logger.info(f"> Respond chat #{chat_id}")
+
+
+def button(bot, update):
+    logger.info(f"> Button chat #{chat_id}")
+
+    query = update.callback_query
+
+    bot.edit_message_text(text="ok. Ill do a {} for you".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+    if query.data == 'Collage':
+        bot.send_message(chat_id=update.message.chat_id, text=f"ok. Ill do a {text} for you")
+
+    if query.data == 'Calender':
+        bot.send_message(chat_id=update.message.chat_id, text=f"ok. Ill do a {text} for you")
+
+    if query.data == 'Greeting Card':
+        bot.send_message(chat_id=update.message.chat_id, text=f"ok. Ill do a {text} for you")
+
+
+def share(bot, update):
+    logger.info(f"> Share chat #{chat_id}")
+
+    chat_id = update.message.chat_id
+
+    bot.send_message(chat_id=chat_id, text="Send this link to your friends")
+    bot.send_message(chat_id=chat_id, text=f" https://telegram.me/{secret_settings.BOT_NAME}?start={chat_id}")
+
+
+def photo(bot, update):
+    logger.info(f"> Photo chat #{chat_id}")
+
+    chat_id = update.message.chat_id
+    file_id = update.message.photo[-1].file_id
+    file_path = bot.getFile(file_id)['file_path']
+    logger.info(f"= Got on chat #{chat_id}: add photo!")
+    bot.sendMessage(chat_id=chat_id, text="added succesfull")
+    if dicargs[chat_id]:
+        data.save_image(file_path, dicargs[chat_id])
+    else:
+        data.save_image(file_path, chat_id)
+
+
 def finish(bot, update):
     chat_id = update.message.chat_id
     logger.info(f"> end chat #{chat_id}")
@@ -38,27 +88,6 @@ def finish(bot, update):
     lst = data.load_image(chat_id)
     mergeimage.create_collage(lst).save(f"{chat_id}_collage.jpg")
 
-def respond(bot, update):
-    chat_id = update.message.chat_id
-    text = update.message.text
-    logger.info(f"= Got on chat #{chat_id}: {text!r}")
-    response = text.replace("7", "💣")
-    bot.send_message(chat_id=update.message.chat_id, text=response)
-
-def button(bot, update):
-    query = update.callback_query
-    bot.edit_message_text(text="ok. Ill do a {} for you".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
-
-
-def photo(bot, update):
-    chat_id = update.message.chat_id
-    file_id = update.message.photo[-1].file_id
-    file_path = bot.getFile(file_id)['file_path']
-    logger.info(f"= Got on chat #{chat_id}: add photo!")
-    bot.sendMessage(chat_id=chat_id, text="added succesfull")
-    data.save_image(file_path, chat_id)
 
 photo_handler = MessageHandler(Filters.photo, photo)
 dispatcher.add_handler(photo_handler)
@@ -74,5 +103,8 @@ dispatcher.add_handler(echo_handler)
 
 logger.info("Start polling")
 updater.start_polling()
+
+share_handler = CommandHandler('share', share)
+dispatcher.add_handler(share_handler)
 
 updater.dispatcher.add_handler(CallbackQueryHandler(button))
