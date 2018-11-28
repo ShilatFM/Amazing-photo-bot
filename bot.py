9f33b78d8e55d76ab28723bf9e2516c56ff2a3b7
Giti Vebber <giti.vebber03@gmail.com>
2018-11-28 13:07:08 +0200

    aa

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import secret_settings
-
 import logging
 
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-import secret_settings
 
 logging.basicConfig(
     format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
@@ -21,8 +21,14 @@ dispatcher = updater.dispatcher
 def start(bot, update):
     chat_id = update.message.chat_id
     logger.info(f"> Start chat #{chat_id}")
-    bot.send_message(chat_id=chat_id, text="💣 Welcome! 💣")
+    bot.send_message(chat_id=chat_id, text="Welcome !!!")
+    keyboard = [[InlineKeyboardButton("Album", callback_data='Album'),
+                 InlineKeyboardButton("Greeting Card", callback_data='Greeting Card'),
+                InlineKeyboardButton("Collage", callback_data='Collage')]]
 
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 def respond(bot, update):
     chat_id = update.message.chat_id
@@ -31,6 +37,12 @@ def respond(bot, update):
     response = text.replace("7", "💣")
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
+def button(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text="ok. Ill do a {} for you".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
 
 
 def photo_handler(bot, update):
@@ -51,7 +63,8 @@ dispatcher.add_handler(start_handler)
 echo_handler = MessageHandler(Filters.text, respond)
 dispatcher.add_handler(echo_handler)
 
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
 logger.info("Start polling")
 updater.start_polling()
 
-print(secret_settings.BOT_TOKEN)
