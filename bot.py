e52d1f4d4c18a3a4c351b5656b076bdbb7447277
Shilat Feuchtwanger Myzel <shilat.myzel@gmail.com>
2018-11-28 10:09:29 +0200

    bot

--- bot.py
+++ bot.py
@@ -1,5 +1,57 @@
 import secret_settings
 
-# YOUR BOT HERE
+import logging
+
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater
+
+import secret_settings
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=secret_settings.BOT_TOKEN)
+dispatcher = updater.dispatcher
+
+
+def start(bot, update):
+    chat_id = update.message.chat_id
+    logger.info(f"> Start chat #{chat_id}")
+    bot.send_message(chat_id=chat_id, text="💣 Welcome! 💣")
+
+
+def respond(bot, update):
+    chat_id = update.message.chat_id
+    text = update.message.text
+    logger.info(f"= Got on chat #{chat_id}: {text!r}")
+    response = text.replace("7", "💣")
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+
+def photo_handler(bot, update):
+    file = bot.getFile(update.message.photo.file_id)
+    # print("file_id: " + str(update.message.photo.file_id))
+    file.download('photo.jpg')
+    bot.send_message("photo send successfully")
+
+# updater = Updater(token='my token')
+# dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.photo, photo_handler))
+
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, respond)
+dispatcher.add_handler(echo_handler)
+
+logger.info("Start polling")
+updater.start_polling()
 
 print(secret_settings.BOT_TOKEN)
